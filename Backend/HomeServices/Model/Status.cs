cdc29d1f0abc0bc8e1bcb27bcbf1044926a493ae
Aishwarya Dhongdi <dhongdiaishwarya04@gmail.com>
2024-08-01 17:49:45 +0530

    model worker status payment added

---
+++ Backend/HomeServices/Model/Status.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeService.Models
+{
+    public class Status
+    {
+        [Key]
+        public int StatusID {  get; set; }
+        
+        [StringLength(25)]
+        public string StatusDesc { get; set; }
+
+        public ICollection<Booking>Bookings{ get; set; }
+
+        
+        }
+
+    
+}
---
+++ Backend/HomeServices/Model/Wpayment.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeService.Models
+{
+    public class Wpayment
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int WpaymentID { get; set; }
+
+        [Required]
+        public int WorkerID { get; set; }
+        [ForeignKey("WorkerID")]
+        public Worker Worker { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal WpaymentAmount { get; set; }
+
+        [Required]
+        public DateTime WpaymentDate { get; set; }
+    }
+
+}
