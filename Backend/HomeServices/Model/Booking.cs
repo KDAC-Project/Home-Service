6e4aa29e92733761187edad194419284bebbfc6f
kd3-Srijan-83611 <srijanchandrakar1.sc@gmail.com>
2024-08-02 15:47:03 +0530

    Entities-Model-Booking-Rating files uploaded

---
+++ Backend/HomeServices/Model/Booking.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HomeService.Models
+{
+    public class Booking
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int BookingId { get; set; }
+
+        public int CustomerId { get; set; }
+        [ForeignKey("CustomerId")]
+        public Customer Customer { get; set; }
+
+        public int WorkerId { get; set; }
+        [ForeignKey("WorkerId")]
+        public Worker Worker { get; set; }
+
+
+        public int ServiceId { get; set; }
+        [ForeignKey("ServiceId")]
+        public Service Service { get; set; }
+
+        [Required]
+        public DateTime BookingDate { get; set; }
+
+        public int StatusId { get; set; }
+
+        [ForeignKey("StatusId")]
+        public Status Status { get; set; }
+
+        public int PaymentId { get; set; }
+
+
+        public class ProjectDBContext : DbContext
+        {
+            public ProjectDBContext(DbContextOptions options) : base(options)
+            {
+
+            }
+
+            protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            {
+                base.OnConfiguring(optionsBuilder);
+            }
+            public DbSet<Booking> Bookings { get; set; }
+
+        }
+    }
+}
+
