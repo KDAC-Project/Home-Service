1fab441958fb3bb4c8e9b406acb18f2268e4cb8f
KD3-Kishan-83613 <kishanv511@gmail.com>
2024-08-03 18:08:25 +0530

    model-cust_modified-dto-custdto-added-all_files_integrated

--- Backend/HomeServices/Model/Customer.cs
+++ Backend/HomeServices/Model/Customer.cs
@@ -1,5 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HomeServices.Model
 {
@@ -7,6 +8,7 @@ namespace HomeServices.Model
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+
         public int CustomerID { get; set; }
 
         [Required]
@@ -19,10 +21,18 @@ namespace HomeServices.Model
         public string Email { get; set; }
 
         [Required]
+        [StringLength(100)]
+        
+        public string Password { get; set; }
+
+
+        [Required]
         [StringLength(15)]
         public string Phone { get; set; }
 
         public string Address { get; set; }
+
+
 
         public ICollection<Booking> Bookings { get; set; }
 
